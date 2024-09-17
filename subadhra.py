ee21a9d4b8c641c95f6f31ee739243cb578d2d92
codepipe <47555726+codepipe@users.noreply.github.com>
2024-09-17 09:23:20 +0530

    Update subadhra.py

--- subadhra.py
+++ subadhra.py
@@ -1,1 +1,21 @@
 we should leaarn basic git commands, its a mandatory for DA employees
+yes, following commands need to know 
+git commands:
+============
+git clone
+git fetch
+git pull
+git push
+git commit
+git config 
+git log
+git reflog
+git init
+git stash
+git revert
+git reset
+git diff
+git branch
+git checkout
+git cherry-pick
+git tag
